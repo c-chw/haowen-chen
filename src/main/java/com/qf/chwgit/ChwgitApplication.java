ceb6084dffa4a6d257d63fe42ff874d7c0a41bfe
haowewn-chen <1030692969@qq.com>
2025-03-24 19:19:38 +0800

    Init

---
+++ src/main/java/com/qf/chwgit/ChwgitApplication.java
@@ -0,0 +1,13 @@
+package com.qf.chwgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChwgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChwgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qf/chwgit/ChwgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.chwgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChwgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
